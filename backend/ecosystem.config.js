55bd2a33a9e1728f5f7fe46c1b100c64dc7a0801
Ildar <misterildar@gmail.com>
2024-03-17 12:53:50 +0300

    deploy

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_GITHUB_PUTH, DEPLOY_REF = 'origin/master',
+} = process.env;
+
+module.exports = {
+  apps: [{
+    name: 'backend',
+    script: './dist/app.js',
+  }],
+
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_GITHUB_PUTH,
+      path: DEPLOY_PATH,
+      'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'post-deploy': 'cd backend && npm i && npm run build',
+    },
+  },
+};
